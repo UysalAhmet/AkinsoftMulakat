217bd5ddb2ee98a120a436b026c953db10151d54
yasemin4480 <56485417+UysalAhmet@users.noreply.github.com>
2021-06-23 16:28:47 +0300

    .

---
+++ Akınsoft/Akınsoft/Islem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akınsoft
+{
+    class Islem
+    {
+        public int baslangic;
+        public int bitis;
+        public string seri;
+        public int sonuc;
+
+
+      
+    }
+}
---
+++ Akınsoft/Akınsoft2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Akınsoft2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            HttpResponseMessage response =await client.GetAsync("http://eliza.akinsoft.net/module/api/programapi/programdetayi?prog_kodu=ISC1");
+            string result = await response.Content.ReadAsStringAsync();
+			JsonToTree.LoadJsonToTreeView(this.treeView1, result);
+           
+        }
+	
+	}
+}
